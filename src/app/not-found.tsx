91cbbf78ddf8d876206af32d65d1b742a69602ef
jini815 <jinijoa0@yahoo.co.jp>
2024-11-11 21:52:23 +0900

    CIDR 機能追加

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,20 @@
+import Link from 'next/link'
 import React from 'react'
 
 const pages = () => {
   return (
-    <div>Not Found Page</div>
+    <div style={{
+      display: 'flex',
+      flexDirection: 'column',
+      gap: '1rem',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      textAlign: 'center',
+    }}>
+      <h2> Not Found Page !!  </h2>
+      <Link href="/"> go Home </Link>
+    </div>
   )
 }
 
