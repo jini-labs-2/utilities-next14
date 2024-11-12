c2049ac859645000ed6572ad9df0e38f55d9bf67
jini815 <jinijoa0@yahoo.co.jp>
2024-11-12 20:50:58 +0900

    add calender, use iframe

---
+++ src/app/iframes/calendar/page.tsx
@@ -0,0 +1,15 @@
+
+export default function Calender() {
+  return (
+    <div>
+      <iframe 
+        src="https://twonine.net/calendar"
+        frameBorder='0'
+        width='100%'
+        height='800px'
+        style={{overflow: 'auto'}}
+      >
+      </iframe>
+    </div>
+  );
+}
