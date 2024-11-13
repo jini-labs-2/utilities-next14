e3de56fd1f6f615186b368173bfa2dd07b67867b
jini815 <jinijoa0@yahoo.co.jp>
2024-11-14 08:30:03 +0900

    ビルド環境設定

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  distDir: 'dist',
+  cleanDistDir: 'dist',
+  trailingSlash: true,
+};
 
 export default nextConfig;
