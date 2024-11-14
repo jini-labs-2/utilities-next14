febf7d27a87c92d350009e57ced6bb6863eadab7
jini815 <jinijoa0@yahoo.co.jp>
2024-11-14 11:53:35 +0900

    active menu表示

--- src/app/components/pages/Navbar.tsx
+++ src/app/components/pages/Navbar.tsx
@@ -1,26 +1,29 @@
+'use client'
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 interface MenuItem {
   name: string,
   path: string,
+  active?: boolean,
   ready?: boolean,
 }
 
 const menuItems: MenuItem[] = [
-  { name: 'Home', path: '/'},
-  { name: 'CIDR', path: '/cidr'},
-  { name: 'timestamp', path: '/timestamp', ready: true},
-  { name: 'UUID v1', path: '/ids/uuidv1'},
-  { name: 'UUID v3', path: '/ids/uuidv3'},
-  { name: 'UUID v4', path: '/ids/uuidv4'},
-  { name: 'UUID v5', path: '/ids/uuidv5'},
-  { name: 'LID', path: '/ids/lid'},
-  { name: 'ULID', path: '/ids/ulid'},
-  { name: 'ShortID', path: '/ids/shortid'},
-  { name: 'CUID', path: '/ids/cuid'},
+  { name: 'CIDR', path: '/cidr/'},
+  { name: 'UUID v1', path: '/ids/uuidv1/'},
+  { name: 'UUID v3', path: '/ids/uuidv3/'},
+  { name: 'UUID v4', path: '/ids/uuidv4/'},
+  { name: 'UUID v5', path: '/ids/uuidv5/'},
+  { name: 'LID', path: '/ids/lid/'},
+  { name: 'ULID', path: '/ids/ulid/'},
+  { name: 'ShortID', path: '/ids/shortid/'},
+  { name: 'CUID', path: '/ids/cuid/'},
+  { name: 'Timestamp', path: '/timestamp/', ready: true},
 ];
 
 export default function Navbar() {
+  const pathName = usePathname();
   return (
     <nav className="nav">
       <ul>
@@ -29,7 +32,7 @@ export default function Navbar() {
             return (
               <li key={menu.name}>
                 <Link href={linkPath}>
-                  <span className="nav__name"> {menu.name} </span>
+                  <span className={`nav__name ${menu.path === pathName || menu.active ? 'active' : ''}`}> {menu.name} </span>
                 </Link>
               </li>
             )
